05e251ddae287ed41006cce1ed6bf1483397f6bb
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-04-26 14:33:53 +0530

    add, getbyEmail, getallusers apis added

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public ResponseEntity<String> getUser() {
@@ -20,5 +26,23 @@ public class UserController {
     public ResponseEntity<String> getMe() {
         String result = "Name: Kavindu, Age: 22, Address: Colombo-07";
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/all-users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        User savedUser = userService.addUser(user);
+        return ResponseEntity.ok(savedUser.getEmail() + ": user saved!!!");
+    }
+
+    @GetMapping("/get-by-email")
+    public ResponseEntity<User> getUserByEmail(@RequestParam("email") String email) {
+        User user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(user);
     }
 }
