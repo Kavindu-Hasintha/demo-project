8f777f5f4248f4809b2f39aa05743726d94ed8ea
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-04-26 11:50:55 +0530

    add user api

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/user")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> getUser() {
+        String result = "User is demo, and welcome!";
+
+        return ResponseEntity.ok(result);
+    }
+}
